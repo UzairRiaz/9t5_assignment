61012b95e81c6e8fc493c69471efdcec4fdf22b3
uzairriaz <uzairriaz.pk@gmail.com>
2022-06-23 14:42:36 +0500

    Jun 23

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,13 +7,13 @@ const { getStorage } = require("firebase/storage");
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCogjVnh3aHaLxp4YeAhO718UWScXuPws0",
-    authDomain: "t5-assginment.firebaseapp.com",
-    projectId: "t5-assginment",
-    storageBucket: "t5-assginment.appspot.com",
-    messagingSenderId: "165655687598",
-    appId: "1:165655687598:web:2b0a38bd0df1e01bfc183f",
-    measurementId: "G-7KR62E9MND"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId,
 };
 
 // Initialize Firebase
